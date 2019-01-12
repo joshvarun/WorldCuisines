f3df4f1449ab24f09bf12b7d36437ce9f72f292d
Varun Joshi <joshvarun@gmail.com>
2019-01-12 17:47:15 +0530

    Setup database

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,66 @@
 # Setup Database for Udacity Full Stack Web Development Nanodegree
 # Item Catalog
 # Written: January 12, 2019 by Varun Joshi
+
+import os, sys
+from sqlalchemy import Column, ForeignKey, String, Integer, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class User(base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    profileImage = Column(String(250))
+
+
+class Category(base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    created_by = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id'            : self.id,
+            'name'          : self.name,
+            'description'   : self.description,
+            'created_by'    : self.created_by,
+        }
+    
+
+
+class Item(base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    created_by = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id'            : self.id,
+            'name'          : self.name,
+            'description'   : self.description,
+            'category_id'   : self.category_id,
+            'created_by'    : self.created_by,
+        }
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
